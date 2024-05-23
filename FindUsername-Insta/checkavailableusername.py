ce62b7422d6ea6fc2dc8fa17a72219a4f4a45cc9
tarun-ainampudi <tarunainampudi@gmail.com>
2024-05-24 02:25:04 +0530

    Yes

--- FindUsername-Insta/checkavailableusername.py
+++ FindUsername-Insta/checkavailableusername.py
@@ -1,25 +1,5 @@
 import requests
-username=input("Enter Username:")
-def print_status_message(status_code):
-    if isinstance(status_code, int):
-        if status_code == 200:
-            print("The request was successful (200 OK).")
-        elif status_code == 404:
-            print("The requested resource was not found (404 Not Found).")
-        elif status_code == 500:
-            print("The server encountered an internal error (500 Internal Server Error).")
-        elif 200 <= status_code < 300:
-            print(f"Success with status code {status_code}.")
-        elif 300 <= status_code < 400:
-            print(f"Redirection with status code {status_code}.")
-        elif 400 <= status_code < 500:
-            print(f"Client error with status code {status_code}.")
-        elif 500 <= status_code < 600:
-            print(f"Server error with status code {status_code}.")
-        else:
-            print(f"Received unexpected status code {status_code}.")
-    else:
-        print(status_code)
+username=input("Enter Username:").lower().split()
 def check_status_code_insta(url,username):
     try:
         response = requests.get(url)
@@ -29,9 +9,13 @@ def check_status_code_insta(url,username):
             print(f'Username : "{username}" is not Available')
             print(f"Refer Profile Link : {url}")
         else:
-            print_status_message(response.status_code)
+            print("Error Occured")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 #instagram
-url = "https://www.instagram.com/"+username
-check_status_code_insta(url,username)
+if len(username)==1:
+    username=username[0]
+    url = "https://www.instagram.com/"+username
+    check_status_code_insta(url,username)
+
+    
