f503df10cbc22bcffe59794b913173a7eab25093
tarun-ainampudi <tarunainampudi@gmail.com>
2024-05-24 10:58:43 +0530

    mod

--- NameGen-Insta/learn.py
+++ NameGen-Insta/learn.py
@@ -2,17 +2,10 @@ import requests
 name="tarun"
 for i in range(100):
     j=f"{i:02}"
-    f=open(name+".txt","a+")
-    f.write(name+j+"\n")
-    f.close()
-file=open(name+".txt","r")
-lines=file.readlines()
-file.close()
-for i in lines:
-    username=i
+    username=j
     url = "https://www.instagram.com/"+username
     response = requests.get(url)
     if "<title>Instagram</title>" in str(response.content) and username!="instagram":
-           f=open(name+"-available.txt","a+")
-           f.write(username)
-           f.close()
+          f=open(name+".txt","a+")
+          f.write(name+j+"\n")
+          f.close()
